89ebc8da3963ba56e9bfeb1a9583723525a82649
top-secret666 <>
2025-05-30 08:13:23 +0300

    git commit -m "feat: 1_versions_6 (super) "

--- src/main/service/exceptions/__init__.py
+++
@@ -1,5 +0,0 @@
-from .validation import ValidationException
-from .database import DatabaseException
-from .business import BusinessException
-
-__all__ = ['ValidationException', 'DatabaseException', 'BusinessException']
--- src/main/service/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .developer import DeveloperValidator
-from .project import ProjectValidator
-from .task import TaskValidator
-
-__all__ = ['DeveloperValidator', 'ProjectValidator', 'TaskValidator']
